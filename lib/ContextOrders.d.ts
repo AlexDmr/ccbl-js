273341f659bb58d9c0428b190189c1dcbf131499
AlexD <alexandre.demeure@imag.fr>
2019-02-27 11:06:00 +0100

    cleaning 3

--- lib/ContextOrders.d.ts
+++
@@ -1,3 +0,0 @@
-import { CCBLContextInterface } from "./ContextInterface";
-export declare type FCT_CONTEXT_ORDER = (root: CCBLContextInterface, priority?: number, doneContexts?: CCBLContextInterface[]) => number;
-export declare let StructuralOrder: FCT_CONTEXT_ORDER;
--- lib/testsProg/domicube.d.ts
+++
@@ -1,2 +0,0 @@
-import { HumanReadableProgram } from "../ProgramObjectInterface";
-export declare const domicube: HumanReadableProgram;
