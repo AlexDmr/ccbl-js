f015afe6429236e3c4838489cab323c6d936d2d3
AlexDmr <hehehe35>
2019-03-05 12:25:39 +0100

    Added another exemple rootProgAvatar

--- lib/AllenEndWith.d.ts
+++
@@ -1,9 +0,0 @@
-import { AllenType } from "./AllenInterface";
-import { CCBLAllen } from "./Allen";
-import { CCBLContextStateAny, CCBLContextStateEndWith } from "./ContextState";
-export declare class CCBLAllenEndWith extends CCBLAllen {
-    parent: CCBLContextStateAny;
-    children: CCBLContextStateEndWith[];
-    constructor(parent: CCBLContextStateAny, children?: CCBLContextStateEndWith[]);
-    getAllenType(): AllenType;
-}
--- lib/testsProg/rootProg_1.d.ts
+++
@@ -1,2 +0,0 @@
-import { HumanReadableProgram } from "../ProgramObjectInterface";
-export declare const rootProgDescr_1: HumanReadableProgram;
