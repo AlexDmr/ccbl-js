629d6c16ca58dd17e8f32e89d5c4e89d4f73b662
AlexDmr <demeurea@SurfacePro7Ng.localdomain>
2020-03-06 16:38:02 +0100

    update

--- lib/ChannelAction.d.ts
+++ lib/ChannelAction.d.ts
@@ -2,6 +2,7 @@ import { CCBLContext } from "./Context";
 import { ChannelActionInterface } from "./ChannelActionStateEventInterface";
 import { CCBLEmitterValue } from "./EmitterValue";
 import { ChannelInterface, ChannelJSON } from "./ChannelInterface";
+import { CCBLEmitterValueInterface } from "./EmitterValueInterface";
 export declare type ChannelActionJSON = {
     type: string;
     channel: ChannelJSON;
@@ -12,10 +13,12 @@ export declare class ChannelAction<T> implements ChannelActionInterface {
     protected active: boolean;
     private attaching;
     private context;
+    private isActivated;
     cbEmitterChange: any;
     constructor(channel: ChannelInterface<T>);
     dispose(): void;
     toJSON(): ChannelActionJSON;
+    getIsActivated(): CCBLEmitterValueInterface<boolean>;
     isChannelActionState(): boolean;
     isChannelActionEvent(): boolean;
     applyTo(V: T): T;
--- lib/ChannelActionStateEventInterface.d.ts
+++ lib/ChannelActionStateEventInterface.d.ts
@@ -1,12 +1,14 @@
 import { CCBLExpressionInExecutionEnvironment } from "./CCBLExpressionInExecutionEnvironment";
 import { ChannelInterface } from "./ChannelInterface";
 import { CCBLEnvironmentExecutionInterface } from "./ExecutionEnvironmentInterface";
+import { CCBLEmitterValueInterface } from "./EmitterValueInterface";
 export interface ChannelActionInterface {
     dispose(): any;
     getPriority(): number;
     getChannel(): ChannelInterface<any>;
     isChannelActionEvent(): boolean;
     isChannelActionState(): boolean;
+    getIsActivated(): CCBLEmitterValueInterface<boolean>;
 }
 export interface ChannelActionEventInterface extends ChannelActionInterface {
     getValueGetter(): CCBLExpressionInExecutionEnvironment<any>;
