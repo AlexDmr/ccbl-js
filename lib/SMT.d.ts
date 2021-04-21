268e0c911e9c352353f57b8df56df3d16bd7d8f5
Alexandre Demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2021-04-21 09:48:31 +0200

    update

--- lib/SMT.d.ts
+++ lib/SMT.d.ts
@@ -1,21 +1,28 @@
 import { MathJsStatic, MathNode } from "mathjs";
-import { HumanReadableStateContext, HumanReadableProgram } from "./ProgramObjectInterface";
+import { HumanReadableStateContext, HumanReadableProgram, HumanReadableStateAction, ContextOrProgram } from "./ProgramObjectInterface";
+export declare const mathjs: Partial<MathJsStatic>;
 export interface SMTscript {
     script: string;
     ids: string[];
 }
-export interface ContextDependencies {
+export interface ContextSMTDescr {
     context: HumanReadableStateContext;
-    parent: ContextDependencies;
-    variables: string[];
-}
-export declare function F(P: HumanReadableProgram, C: HumanReadableStateContext): {
-    context: HumanReadableStateContext;
+    channelDependencies: string[];
     satisfaisable: string;
     réfutable: string;
-};
-export declare const mathjs: Partial<MathJsStatic>;
-export declare function getSMTExpr(P: HumanReadableProgram, expr: string): string;
+    actOnChannels: string[];
+}
+export interface ExpressionSMT {
+    dependencies: string[];
+    SMT: string;
+}
+export declare function getContextStateSMT(P: HumanReadableProgram, C: HumanReadableStateContext): ContextSMTDescr;
+export declare function getContextSMTdescrFromProg(P: HumanReadableProgram): string;
+export declare function getActionsSMT(P: HumanReadableProgram, actions: HumanReadableStateAction[]): string;
+export declare function canActivateAndDesactivate(P: HumanReadableProgram, C: HumanReadableStateContext, ancestorActions: HumanReadableStateAction[]): string;
+export declare function getStateContextsFrom(L: ContextOrProgram[]): HumanReadableStateContext[];
+export declare function getProgramDeclarations(P: HumanReadableProgram): string;
+export declare function getSMTExpr(P: HumanReadableProgram, expr: string): ExpressionSMT;
 export declare function getSMTforContext(P: HumanReadableProgram, C: HumanReadableStateContext): SMTscript;
 export declare function getSMT(P: HumanReadableProgram): string;
 export declare function mathNodeToSMT(P: HumanReadableProgram, node: MathNode): string;
